e458e2fb263a695f30fe19ce3d58dd8c02a73b04
mapy0 <90832266+mapy0@users.noreply.github.com>
2021-12-05 08:29:43 +0000

    [Still in]device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :aim_w, :aim_bf, :aim_mus, :aim_sm, :height, :birth, :age, :sex, :self_introduction, :image_id])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :members, controllers: {
+    registrations: 'members/registrations',
+    sessions: 'members/sessions',
+   }
+
+
+   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
