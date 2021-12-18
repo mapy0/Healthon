cff3bb6e0244172962f6b4b440301e41c8424aca
mapy0 <90832266+mapy0@users.noreply.github.com>
2021-12-18 07:46:20 +0000

    [implementation]

--- app/assets/javascripts/channels/circles.js
+++ app/assets/javascripts/channels/circles.js
@@ -10,25 +10,32 @@ connected: function() {
   },
 
   received: function(comment) {
+    console.log(comment)
     const comments = document.getElementById('comments')
-    messages.innerHTML += comment
+    comments.innerHTML += comment.message
     //# Called when there's incoming data on the websocket for this channel
   },
 
-  speak: function(comment) {
-    return this.perform ('speak',{comment: comment});
+  speak: function(comment, circle_id, member_id) {
+    console.log(comment)
+    return this.perform ('speak',{comment: comment,circle_id: circle_id,member_id: member_id});
+
   }
 
 });
 
-document.addEventListener('DOMCommentLoaded', function(){
- const input = document.getElementById('chat_input')
+document.addEventListener('DOMContentLoaded', function(){
+ const input = document.getElementById('chat-input')
+ const circle = document.getElementById('circle_id')
+ const member = document.getElementById('member_id')
  const button = document.getElementById('button')
  button.addEventListener('click', function(){
   const comment = input.value
+  const circle_id = circle.value
+  const member_id = member.value
+  App.circle.speak(comment, circle_id, member_id)
 
-  App.circle.speak(comment)
-  Input.value = ""
-  
+  input.value = ""
+
  })
 })
--- app/channels/circle_channel.rb
+++ app/channels/circle_channel.rb
@@ -1,7 +1,6 @@
 class CircleChannel < ApplicationCable::Channel
   def subscribed
-
-    # stream_from "some_channel"
+    stream_from "circle_channel"
   end
 
   def unsubscribed
@@ -9,8 +8,16 @@ class CircleChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-   comment = CirComment.create!(comment: data['comment'])
-   template = ApplicationController.renderer.render(partial: 'comments/comment', locals:{comment: comment})
-   ActionCable.server.broadcast 'circle_channel', template
+    circle = Circle.find(data['circle_id'])
+    user = Member.find(data['member_id'])
+    comment = user.cir_comments.new(comment: data['comment'])
+    comment.circle_id = circle.id
+    comment.save
+  p "------------------------"
+  p comment
+  p user
+  # comment = CirComment.create!(comment: data['comment'])
+   template = ApplicationController.renderer.render(partial: 'members/circles/comment', locals: {comment: comment})
+   ActionCable.server.broadcast 'circle_channel', message: template
   end
 end
